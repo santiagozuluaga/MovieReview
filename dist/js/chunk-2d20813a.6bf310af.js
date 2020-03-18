4435eca14076813d53ff9fd70848db040c680627
Santiago Zuluaga <santiagozh1998@gmail.com>
2020-03-18 14:07:48 -0500

    Dist commit

---
+++ dist/js/chunk-2d20813a.6bf310af.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d20813a"],{a2f9:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},c=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h3",[e._v("Novedades")])])}],r={},u=r,l=t("2877"),s=Object(l["a"])(u,a,c,!1,null,null,null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d20813a.6bf310af.js.map
